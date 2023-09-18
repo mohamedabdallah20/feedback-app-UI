2b495480b85ad77492961054b0957c3e97056d8b
mohamedabdallah20 <mohamedabdala1442000@gmail.com>
2023-09-18 16:02:25 +0300

    fix a deployment issue

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
               }
             ></Route>
             <Route path="/about" element={<AboutPage />} />
-            <Route path="/hammo" element={<HammoPage />} />
+            {/* <Route path="/hammo" element={<HammoPage />} /> */}
           </Routes>
           <AboutIconLink />
           {/* <HammoLink /> */}
